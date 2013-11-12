a4341389c584c77b8984ed44a8a4582394e46244
Stuart Stent <stuart.stent@gmail.com>
2013-11-12 13:53:06 -0500

    setting this up a clean setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Vagrant_Openstack
 =================
+
+Basic 3 node puppet cluster
+	1 Puppet node
+	1 controller
+	1 compute
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,65 +1,20 @@
 node puppet {
 
-	#ensure git is installed
- package { 'puppetlabs-release-6-7':
-     provider => 'rpm',
-     ensure => installed,
-     source => "http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm";
-    'git':
+ package { 
+  'puppetlabs-release-6-7':
+    provider => 'rpm',
+    ensure => installed,
+    source => "http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm";
+   'git':
 		ensure 		=> 'present';
-	'puppet-server':
+   'puppet-server':
 		require => Package["puppetlabs-release-6-7"],
 		ensure 		=> 'present';
-  'rubygem-rake':
+   'rubygem-rake':
     ensure  => 'present';
 	}
 
 
-
-
-vcsrepo { 
-'/etc/puppet/modules/openstack':
-  require => Package["puppet-server"],
-  ensure   => latest,
-  provider => git,
-  source   => 'https://sstent:farscape5@github.com/stratustech/puppet-openstack.git',
-  notify   => File["/etc/puppet/modules/openstack"];
-'/etc/puppet/modules/stratus':
-  require => Package["puppet-server"],
-  ensure   => latest,
-  provider => git,
-  source   => 'https://sstent:farscape5@github.com/stratustech/POC_ALPHA_stratusmodule.git';
-'/etc/puppet/manifests':
-  require => [Package["puppet-server"],File['/etc/puppet/manifests']],
-  ensure   => latest,
-  provider => git,
-  source   => 'https://sstent:farscape5@github.com/stratustech/POC_ALPHA_puppet_manifests.git';
-  '/etc/puppet/modules/rabbitmq':
-  require => Package["puppet-server"],
-  ensure   => latest,
-  provider => git,
-  source   => 'https://github.com/gergnz/puppetlabs-rabbitmq.git';
-  '/etc/puppet/modules/horizon':
-  require => Package["puppet-server"],
-  ensure   => latest,
-  provider => git,
-  source   => 'https://sstent:farscape5@github.com/stratustech/puppet-horizon.git';
- }
-
-file { "/etc/puppet/modules/openstack":
-  require => Package["puppet-server"],
-  ensure => "directory",
-  owner  => "root",
-  group  => "root",
-  mode   => 755,
-  recurse => true,
-  notify  => Exec["sudo rake modules:clone"];
-"/etc/puppet/manifests":
-  require => Package["puppet-server"],
-  before => Vcsrepo['/etc/puppet/manifests'],
-  force => true,
-  backup => false,
-  ensure => "absent";
 "/etc/puppet/autosign.conf":
   require => Package["puppet-server"],
   owner  => "root",
@@ -68,17 +23,11 @@ file { "/etc/puppet/modules/openstack":
   content => "*";
 }
 
-exec {"sudo rake modules:clone":
-			require => [Package["rubygem-rake"],Vcsrepo['/etc/puppet/modules/horizon','/etc/puppet/modules/rabbitmq','/etc/puppet/manifests','/etc/puppet/modules/openstack']],
-			cwd => "/etc/puppet/modules/openstack",
-			path => ["/usr/local/bin","/bin","/usr/bin","/usr/local/sbin","/usr/sbin","/sbin","/home/vagrant/bin"];
-		}
-
 service { "iptables":
         ensure => "stopped",
         enable => false;
         "puppetmaster":
-        require => [File["/etc/puppet/autosign.conf"],Exec["sudo rake modules:clone"]],
+        require => File["/etc/puppet/autosign.conf"],
         ensure => "running",
         enable => true;
       }
@@ -98,28 +47,18 @@ host { 'controller.vagrant.info':
 
 node controller {
 
-	#ensure git is installed
- package { 'puppetlabs-release-6-7':
+package { 
+  'puppetlabs-release-6-7':
      provider => 'rpm',
      ensure => installed,
      source => "http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm";
 	'puppet':
 		require => Package["puppetlabs-release-6-7"],
 		ensure 		=> 'present';
-	}
+}
 
-service { "iptables":
-        ensure => "stopped",
-        enable => false;
-        "puppet":
-        require => Package["puppet"],
-        ensure => "running",
-        enable => true;
-      }
-
-
-
-host { 'puppet.vagrant.info':
+host { 
+    'puppet.vagrant.info':
     ip => '192.168.33.10',
     host_aliases => 'puppet';
     'compute1.vagrant.info':
@@ -148,8 +87,6 @@ service { "puppet":
         ensure => "running",
         enable => true;
       }
-
-
 
 host { 'puppet.vagrant.info':
     ip => '192.168.33.10',
